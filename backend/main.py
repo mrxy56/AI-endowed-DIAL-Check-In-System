248d57b0a1f4bee2318fef58818f5087cc9e3206
mrxy56 <mrxy56@gmail.com>
2022-04-26 11:54:33 -0400

    final

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,21 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
-clf = LogisticRegression(C= 1000, penalty= ‘l1’)
-clf.fit(x_train,y_train)
-pred = clf.predict(x_test)
-print(“Non Zero weights:”,np.count_nonzero(clf.coef_))
+from NN import Net
+from SVM import SVM_DUAL
+
+train_df = pd.read_excel('covid.xlsx')
+model = LogisticRegression(solver='liblinear')
+valid_col = ['Fever', 'Cough', 'Fatigue', 'Breathing', 'Ache', 'Headache', 'LossTaste']
+target_col = 'Pred'
+tr = train_df[valid_col]
+y = train_df[target_col]
+print(tr[:30])
+model.fit(tr[:30], y[:30])
+X_train = tr[:30]
+train_preds = model.predict(X_train)
+print(train_preds)
+#net = Net() #TBA
+#svm = SVM_DUAL() #TBA
